88af28561a73135eb51003f115aaa16d6e3ef932
liad7 <liadgola7@gmail.com>
2023-03-08 13:37:01 +0200

    home page examples column

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 function HomePage() {
     return (
-        <section className="home-page">
-            <h1>ChatGPT-messenger</h1>
+        <section className="flex flex-col items-center justify-center h-screen px-2 text-white">
+            <h1 className="text-5xl font-bold mb-20">ChatGPT-messenger</h1>
+            <div>
+                <div>
+                    <div className="flex flex-col items-center justify-center mb-5">
+                        <h2>Examples</h2>
+                    </div>
+                    <div className="space-y-2">
+                        <p className="text-info">"Explain something to me"</p>
+                        <p className="text-info">"What is the different between a dog and a cat?"</p>
+                        <p className="text-info">"What is the color of the sun?"</p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
