81805231974e815a99ebce208866fb6632379039
liad7 <liadgola7@gmail.com>
2023-03-08 18:46:37 +0200

    install next auth

--- cmps/new-chat.tsx
+++ cmps/new-chat.tsx
@@ -1,10 +1,10 @@
 import { PlusIcon } from "@heroicons/react/24/solid"
 
 export function NewChat() {
-return(
-<section className="border-gray-700 border chat-row">
-    <PlusIcon className="h-4 w-4"/>
-    <p>New Chat</p>
-</section>
-)
+    return (
+        <section className="border-gray-700 border chat-row">
+            <PlusIcon className="h-4 w-4" />
+            <p>New Chat</p>
+        </section>
+    )
 }
