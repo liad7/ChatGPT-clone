aa94846678bcfd79be6084817792d586657c8e9d
liad7 <liadgola7@gmail.com>
2023-03-09 13:04:04 +0200

    create new chat func

--- cmps/new-chat.tsx
+++ cmps/new-chat.tsx
@@ -1,8 +1,25 @@
+'use client'
+import { addDoc,collection,serverTimeStamp } from 'firebase/firestore'
 import { PlusIcon } from "@heroicons/react/24/solid"
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import { db } from '../firebase'
 
 export function NewChat() {
+    const router = useRouter()
+const {data:session} = useSession()
+
+const createNewChat = async()=>{
+const doc = await addDoc (collection(db,'users',session?.user?.email,'chats'),{
+    messages:[],
+    userId:session?.user?.email,
+    createdAt: serverTimeStamp()
+})
+router.push(`/chat/${doc.id}`)
+}
+
     return (
-        <section className="border-gray-700 border chat-row">
+        <section onclick={createNewChat} className="border-gray-700 border chat-row">
             <PlusIcon className="h-4 w-4" />
             <p>New Chat</p>
         </section>
