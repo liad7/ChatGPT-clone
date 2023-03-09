95a23375c004867a5d30f731459f55392a28d399
liad7 <liadgola7@gmail.com>
2023-03-09 13:14:41 +0200

    install react firebase hooks

---
+++ app/chat/[id]/page.tsx
@@ -0,0 +1,7 @@
+export function ChatPage() {
+return(
+<section>
+chat
+</section>
+)
+}
--- cmps/new-chat.tsx
+++ cmps/new-chat.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { addDoc,collection,serverTimeStamp } from 'firebase/firestore'
+
+import { addDoc,collection,serverTimestamp } from 'firebase/firestore'
 import { PlusIcon } from "@heroicons/react/24/solid"
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
@@ -11,15 +12,14 @@ const {data:session} = useSession()
 
 const createNewChat = async()=>{
 const doc = await addDoc (collection(db,'users',session?.user?.email,'chats'),{
-    messages:[],
     userId:session?.user?.email,
-    createdAt: serverTimeStamp()
+    createdAt: serverTimestamp()
 })
 router.push(`/chat/${doc.id}`)
 }
 
     return (
-        <section onclick={createNewChat} className="border-gray-700 border chat-row">
+        <section onClick={createNewChat} className="border-gray-700 border chat-row">
             <PlusIcon className="h-4 w-4" />
             <p>New Chat</p>
         </section>
