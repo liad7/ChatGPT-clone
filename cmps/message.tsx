04cdee438d6059f59d2090e63b437122baa6002d
liad7 <liadgola7@gmail.com>
2023-03-11 22:21:15 +0200

    message bg color

--- cmps/message.tsx
+++ cmps/message.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 
 export function Message({ message }: Props) {
-const isChatGPT = message.user.name = 'ChatGPT'
+const isChatGPT = message.user.name === 'ChatGPT'
     return (
         <section className={`py-5 text-white ${isChatGPT && 'bg-[#434654]'}`}>
             <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
